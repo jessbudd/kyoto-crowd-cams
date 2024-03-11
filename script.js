b0605eb30cc04298cacf7c434aeda034bcbc5e18
Jess Budd <jbudd4@gmail.com>
2024-03-11 22:24:36 +1100

    load videos with js and move into separate file

---
+++ script.js
@@ -0,0 +1,56 @@
+const videos = [
+  {
+    id: 'Op-lf2NRMzs',
+    title: 'Arashiyama Bamboo forest',
+  },
+  {
+    id: 'pgqwfZj9HPY',
+    title: 'Fushimi Inari Taisha Shrine',
+  },
+  {
+    id: 'jqtsC5BYlIk',
+    title: 'Saga Arashiyama main street',
+  },
+  {
+    id: 'wuC8wRvXock',
+    title: 'Nishiki Market',
+  },
+  {
+    id: 'S6IkZhhwG4A',
+    title: 'Philosopher’s Path',
+  },
+  {
+    id: 'v9rQqa_VTEY',
+    title: 'Station Bus Terminal',
+  },
+  {
+    id: 'Gxt3YCa2Phc',
+    title: 'Nene no michi (Nene Street)',
+  },
+  {
+    id: 'PXg3ZXgMkGk',
+    title: 'Hanamikoji Street',
+  },
+];
+
+const renderVideos = () => {
+  const videosContainer = document.getElementById('js-videos-container');
+
+  videos.forEach(video => {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube-nocookie.com/embed/${video.id}?&autoplay=1&mute=1&controls=0`;
+    iframe.frameborder = 0;
+    iframe.allowfullscreen = true;
+    iframe.className = 'yt-embed';
+    iframe.title = video.title;
+    const div = document.createElement('div');
+    div.className = 'grid-item';
+    const h2 = document.createElement('h2');
+    h2.textContent = video.title;
+    div.appendChild(h2);
+    div.appendChild(iframe);
+    videosContainer.appendChild(div);
+  });
+};
+
+window.addEventListener('load', renderVideos);
